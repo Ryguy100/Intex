4a5e3bac8a134300459cb8ceb482d8061135554e
Andrew Naumann <35020306+AndresNaumann@users.noreply.github.com>
2023-12-04 10:03:17 -0700

    init commit

--- intex_2023/index.js
+++ intex_2023/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-
 let app = express();
-
 let path = require("path");
 
-const PORT = 3000;
+const PORT = 5500;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/"));
 
 // const knex = require("knex")({
 //     client: "pg",
@@ -20,8 +19,8 @@ app.use(express.urlencoded({extended:true}));
 //     }
 // });
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(PORT, () => console.log("Application has started"));
