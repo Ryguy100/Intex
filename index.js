4942e2da128775b54de9bde1f429b2a36a5e508b
Ryguy100 <ryanjhafen@gmail.com>
2023-12-04 23:45:04 -0700

    added database config

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const knex = require("knex")({
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
     port: process.env.RDS_PORT,
+    ssl: process.env.DB_SSL ? {rejectUnauthorized : false} : false
   },
 });
 
