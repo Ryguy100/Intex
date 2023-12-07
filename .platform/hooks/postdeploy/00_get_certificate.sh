87ab72f3af65a184ddf4d928583c776d3e256284
Ryguy100 <ryanjhafen@gmail.com>
2023-12-06 19:15:49 -0700

    updated certbot dns name

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d intex.ryanhafen.org --nginx --agree-tos --email ryanjhafen@gmail.com
+sudo certbot -n -d provo-mental-health.ryanhafen.org --nginx --agree-tos --email ryanjhafen@gmail.com
